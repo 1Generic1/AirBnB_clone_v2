82da0c7d58ec44cfbe4c407815de45efe523582b
kelvin <kelvin4stunt@gmail.com>
2023-12-27 13:53:20 +0100

    updatedwithfabstaticdopack

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """Generates a .tgz archive from the contents of the web_static folder."""
 from fabric.api import *
 from datetime import datetime
 import os
+
 
 def do_pack():
     """Generates a .tgz archive from the contents of the web_static folder."""
