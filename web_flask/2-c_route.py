e4560c21260bb1eaf6f25ea1ca6f606e58ca9d9d
kelvin <kelvin4stunt@gmail.com>
2024-01-03 12:46:57 +0100

    up

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
-
+"""script that starts a Flask web application"""
 from flask import Flask
-
-
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
 @app.route('/')
 def hello_hbnb():
+    """print web"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb')
 def hbnb():
+    """print web"""
     return 'HBNB'
 
 
 @app.route('/c/<text>')
 def c_text(text):
+    """print C followed by the value of the text variable"""
     return 'C {}'.format(text.replace('_', ' '))
 
 
