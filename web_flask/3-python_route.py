8a0a1c091dd461a27f66f983fa4b237c202ba785
kelvin <kelvin4stunt@gmail.com>
2024-01-04 01:38:43 +0100

    up

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,33 +1,35 @@
 #!/usr/bin/python3
-"""script that starts a Flask web application"""
+""" Script that starts a Flask web application """
 from flask import Flask
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
 @app.route('/')
 def hello_hbnb():
-    """print web"""
+    """ Print Web """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb')
 def hbnb():
-    """print web"""
+    """ Print Web """
     return 'HBNB'
 
 
 @app.route('/c/<text>')
-def c_text(text):
-    """print C followed by the value of the text variable"""
+def c_is_fun(text):
+    """ Print a char C followed by the value of the text variable """
     return 'C {}'.format(text.replace('_', ' '))
 
 
-@app.route('/python/')
+@app.route('/python')
 @app.route('/python/<text>')
-def display_python(text='is cool'):
-    """Displays 'Python ' followed by the value of the text variable."""
-    return 'python {}'.format(text.replace('_', ' '))
+def python_is_cool(text='is cool'):
+    """ Print Python, followed by the value of the text variable,
+    with default value of text: is cool"""
+    return 'Python {}'.format(text.replace('_', ' '))
 
 
 if __name__ == '__main__':
